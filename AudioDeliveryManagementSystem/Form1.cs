19eebb988b9459f72d14a0255628672ac13346b0
RasmusBroborg <43502647+RasmusBroborg@users.noreply.github.com>
2021-09-28 17:04:22 +0200

    Removing message boxes when adding files.

--- AudioDeliveryManagementSystem/Form1.cs
+++ AudioDeliveryManagementSystem/Form1.cs
@@ -39,36 +39,29 @@ namespace AudioDeliveryManagementSystem
             //Loop through all dropped items and display them
             foreach (string file in droppedFiles)
             {
-                string filePath = getFilePath(file);
-                MessageBox.Show($"{filePath} added.");
-                userDraggedFiles.Items.Add(filePath);
+                userDraggedFiles.Items.Add(Path.GetFullPath(file));
             }
         }
 
-        private string getFilePath(string path)
-        {
-            return Path.GetFullPath(path);
-        }
 
         private void browseSystemFiles_Click(object sender, EventArgs e)
         {
-            int size = -1;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             DialogResult result = openFileDialog.ShowDialog(); // Show the dialog.
-            if (result == DialogResult.OK) // Test result.
+            if (result == DialogResult.OK)
             {
                 string file = openFileDialog.FileName;
                 try
                 {
                     string text = File.ReadAllText(file);
-                    size = text.Length;
+
+                    userDraggedFiles.Items.Add(file);
                 }
                 catch (IOException)
                 {
                 }
             }
-            Console.WriteLine(size); // <-- Shows file size in debugging mode.
-            Console.WriteLine(result); // <-- For debugging use.
+            
         }
     }
 }
---
+++ AudioDeliveryManagementSystem/WaveFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace AudioDeliveryManagementSystem
+{
+    class WaveFile
+    {
+        public WaveFileStatus Status { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public long FileSizeInBytes { get; set; }
+        public long DurationInMillieSecs { get; set; }
+        public WaveFile(string filePath)
+        {
+            Status = WaveFileStatus.Unchecked;
+            System.IO.FileInfo file = new System.IO.FileInfo(filePath);
+            FilePath = filePath;
+            FileName = file.Name;
+            FileSizeInBytes = file.Length;
+
+            WaveFileReader reader = new WaveFileReader(filePath);
+            TimeSpan duration = reader.TotalTime;
+            DurationInMillieSecs = duration.Milliseconds;
+        }
+    }
+}
